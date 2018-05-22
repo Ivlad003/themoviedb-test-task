0f18479d122770443534c91f9b58fb4824c5094f
Vlad Kosmach <ivlad003@gmai.com>
2018-05-22 14:37:54 +0300

    Initial Commit

---
+++ themoviedb-test-task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  themoviedb-test-task
+//
+//  Created by Vlad Kosmach on 5/22/18.
+//  Copyright © 2018 Vlad Kosmach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
